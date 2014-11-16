c8ff30f717567ab50a095fc6c2117605c6f38f19
Nate Schickler <nate@schickler.org>
2014-11-16 13:25:01 -0800

    Swap JawPiston solenoids.

--- src/org/usfirst/frc/team294/robot/RobotMap.java
+++ src/org/usfirst/frc/team294/robot/RobotMap.java
@@ -40,8 +40,8 @@ public class RobotMap {
 	public static int kSOL_shifterPiston_reverse = 0;
 	public static int kSOL_shifterPiston_forward = 1;
 	public static int kSOL_jawPistonModule = 1;
-	public static int kSOL_jawPiston_reverse = 2;
-	public static int kSOL_jawPiston_forward = 3;
+	public static int kSOL_jawPiston_reverse = 3;
+	public static int kSOL_jawPiston_forward = 2;
 	public static int kSOL_kickerPistonModule = 1;
 	public static int kSOL_kickerPiston = 4;
 	public static int kSOL_rangeLight = 5;
